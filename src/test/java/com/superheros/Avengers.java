44ec36e27d9083f9a433d89e340f3229536d0eb6
chettrycharan <saransakthivel8118@gmail.com>
2022-09-29 10:33:42 +0530

    marvelll

---
+++ src/test/java/com/superheros/Avengers.java
@@ -0,0 +1,14 @@
+package com.superheros;
+
+public class Avengers {
+private void spiderman() {
+System.out.println("spider");
+}
+private void thor() {
+System.out.println("thunder");
+}
+
+
+
+
+}
